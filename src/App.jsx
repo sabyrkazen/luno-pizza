42371223be0dadb4bc3e48ccf6f46d64f08afa69
Sabyrka <sabyrkazen@gmail.com>
2026-02-19 20:35:06 +0500

    fix: update header title from "React Pizza" to "Luno Pizza" for branding consistency

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
           <div className="header__logo">
             <img width={38} src={Logo} alt="Pizza logo" />
             <div>
-              <h1>React Pizza</h1>
+              <h1>Luno Pizza</h1>
               <p>самая вкусная пицца во вселенной</p>
             </div>
           </div>
